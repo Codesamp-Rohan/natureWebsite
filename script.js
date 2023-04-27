a07d81bcf95ea2f3359b51521390be9379c4bc56
DocYard <rohanchaudhary.bkbiet2022@gmail.com>
2023-04-27 22:23:56 +0530

    1

---
+++ script.js
@@ -0,0 +1,18 @@
+// gsap.to("#natureImgBg1", {
+//     scrollTrigger: {
+//         scrub: true
+//     },
+//     y: -100
+// })
+// gsap.to("#natureImgBg2", {
+//     scrollTrigger: {
+//         scrub: true
+//     },
+//     y: -200
+// })
+gsap.to("#natureImgBg2", {
+    scrollTrigger: {
+        scrub: true
+    },
+    y: -250
+})
